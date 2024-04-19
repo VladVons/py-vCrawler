ed1ba2bee4a9a356a5875c79d23a438fc1fd5f1a
VladVons <vladvons@gmail.com>
2024-04-19 07:51:14 +0300

    MyCommit

--- src/MVC/model/_todo/fmtIns_HistUrlIntoProduct.sql
+++ src/MVC/model/_todo/fmtIns_HistUrlIntoProduct.sql
@@ -1,5 +1,5 @@
-insert into ref_url_product 
-(url_id, sku, mpn, title, category, stock, price, price_old)
+insert into ref_url_product
+(url_id, sku, mpn, title, category, stock, price, price_old, image)
     select
         distinct on (url_id) url_id,
         (parsed_data->>'sku') as sku,
@@ -8,19 +8,21 @@ insert into ref_url_product
         left(parsed_data->>'category', 80) as category,
         (parsed_data->>'stock')::bool as stock,
         (parsed_data->'price'->>0)::decimal as price,
-        (parsed_data->'price_old'->>0)::decimal as price_old
-    from 
+        (parsed_data->'price_old'->>0)::decimal as price_old,
+        left(parsed_data->'images'->>0, 128) as image
+    from
         hist_url
     where
-        (parsed_data is not null) 
-    order by 
+        (parsed_data is not null)
+    order by
         url_id, id desc
 on conflict (url_id) do update
-set 
+set
     sku = excluded.sku,
     mpn = excluded.mpn,
     title = excluded.title,
     category = excluded.category,
     stock = excluded.stock,
     price = excluded.price,
-    price_old = excluded.price_old
+    price_old = excluded.price_old,
+    image = excluded.image
