5894a7a3a45ac080180209619a1eafaab189c2e2
VladVons <vladvons@gmail.com>
2024-08-29 08:38:39 +0300

    MyCommit

--- src/MVC/Collector/model/tables/init/dbData.sql
+++ src/MVC/Collector/model/tables/init/dbData.sql
@@ -2,6 +2,6 @@ INSERT INTO ref_user (id, login, passw, enabled) VALUES
      (0, 'common','none',false);
 
 INSERT INTO ref_user_ext (attr,val,enabled,user_id) VALUES
-     ('max_workers','5',true,0),
+     ('workers_qty','5',true,0),
      ('speed_test_url','https://ping.virtua.cloud/100MB.bin',true,0);
 
