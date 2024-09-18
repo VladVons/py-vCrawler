fcefba2f3abea2dbea5027d3d4d4dad1f8e79adb
VladVons <vladvons@gmail.com>
2024-09-18 10:33:49 +0300

    MyCommit

--- src/vCrawler.sh
+++ src/vCrawler.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 
-py=python3.12
-Dir=~/VirtEnv/$py
+py=python3
+Dir=~/virt/$py
 source $Dir/bin/activate
 
-$py -B vCrawler.py
+
+$py -B vCrawler.py --conf Dev
+#$py -B vCrawler.py --conf Client
+#$py -B vCrawler.py --conf Server
+
