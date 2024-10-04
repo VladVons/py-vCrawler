b610b47241b6e42a182edcca6a48b3bb3c044f8a
VladVons <vladvons@gmail.com>
2024-10-04 16:22:12 +0300

    MyCommit

--- src/MVC/Collector/model/_todo/fmtGet_UnlockNow.sql
+++ src/MVC/Collector/model/_todo/fmtGet_UnlockNow.sql
@@ -1,42 +1,47 @@
-select 
+-- involved urls
+select
   ru.site_id,
   rs.sleep_seconds,
   to_char(ru.unlock_date, 'YYYY-MM-DD HH24:MI:SS') as unlock_date,
   ru.url
-from 
+from
   ref_url ru
-join 
+join
   ref_site rs on
   (rs.id = ru.site_id)
-where 
-  (ru.unlock_date >= now())  
-order by 
+where
+  (ru.unlock_date >= now())
+order by
   url;
 
-select 
+
+-- involved sites
+select
   rs.id,
   rs.sleep_seconds,
   to_char(rs.unlock_date, 'YYYY-MM-DD HH24:MI:SS') as unlock_date,
   rs.url
-from 
+from
   ref_site rs
-where 
-  (rs.unlock_date >= now())  
-order by 
+where
+  (rs.unlock_date >= now())
+order by
   url;
 
-select 
+
+-- not involved
+select
   rs.id,
   rs.sleep_seconds,
   to_char(rs.unlock_date, 'YYYY-MM-DD HH24:MI:SS') as unlock_date,
   rs.url
-from 
+from
   ref_site rs
-where 
+where
   id not in (
     select id
     from ref_site
-    where (unlock_date >= now())  
+    where (unlock_date >= now())
   )
-order by 
+order by
   url;
