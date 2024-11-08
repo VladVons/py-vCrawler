c72346165c81da792ba96bb1fe5345095037cca5
VladVons <vladvons@gmail.com>
2024-11-08 04:18:26 +0200

    MyCommit

--- src/IncP/__init__.py
+++ src/IncP/__init__.py
@@ -3,7 +3,7 @@
 #License:     GNU, see LICENSE for more details
 
 
-__version__ = '1.0.48'
+__version__ = '1.0.49'
 __date__ =  '2024.11.07'
 __author__ = 'Vladimir Vons'
 __email__ = 'VladVons@gmail.com'
