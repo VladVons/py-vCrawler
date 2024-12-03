2365af3378e03b24c8c48249f3431797c4a44b02
VladVons <vladvons@gmail.com>
2024-12-03 15:46:56 +0200

    MyCommit

--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
-from Inc.ParserSpec import TSpecCpu, TSpecRam, TSpecStorage, TSpecOs, TSpecScreen
+from Inc.ParserSpec import TSpecCpu, TSpecRam, TSpecStorage, TSpecOs, TSpecScreen, TSpecFormFactor
 
 
 def Test_01():
@@ -7,24 +7,25 @@ def Test_01():
         '-ram': TSpecRam(),
         '-disk': TSpecStorage(),
         'screen': TSpecScreen(),
-        '-os': TSpecOs()
+        '-os': TSpecOs(),
+        '-form': TSpecFormFactor()
     }
 
-    File = 'Inc/ParserSpec/test/Processor-intel.txt'
+    File = 'Inc/ParserSpec/test/cpu-amd.txt'
     with open(File, 'r', encoding='utf8') as F:
-        Lines = F.readlines()
+        Lines = []
+        for xLine in F.readlines():
+            xLine = xLine.strip()
+            if (xLine) and (not xLine.startswith('-')):
+                Lines.append(xLine)
 
-    for xLine in Lines:
-        xLine = xLine.strip()
-        if (not xLine) or (xLine.startswith('-')):
-            continue
-
-        print(xLine)
+    for Idx, xLine in enumerate(Lines):
+        print(f'{Idx+1}/{len(Lines)}: {xLine}')
         for xKey, xVal in SpecObj.items():
             if (not xKey.startswith('-')):
                 R = xVal.Parse(xLine)
                 if (R):
-                    print(R)
+                    print(xKey, R)
         print()
 
 Test_01()
