816a5f333fad48fe99023fd6d92789b59fbab6fe
VladVons <vladvons@gmail.com>
2024-12-04 00:40:56 +0200

    MyCommit

--- src/Temp/test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from Inc.Misc.Sitemap import TSitemapRead
-
-async def Main():
-    Sitemap = TSitemapRead('https://chipchip.ua/ua')
-    Urls = await Sitemap.Load()
-    pass
-
-asyncio.run(Main())
--- src/test.py
+++ src/test.py
@@ -1,31 +1,4 @@
-from Inc.ParserSpec import TSpecCpu, TSpecRam, TSpecStorage, TSpecOs, TSpecScreen, TSpecFormFactor
+from Inc.ParserSpec.TestAll import TestAll
 
-
-def Test_01():
-    SpecObj = {
-        '-cpu': TSpecCpu(),
-        '-ram': TSpecRam(),
-        '-disk': TSpecStorage(),
-        'screen': TSpecScreen(),
-        '-os': TSpecOs(),
-        '-form': TSpecFormFactor()
-    }
-
-    File = 'Inc/ParserSpec/test/cpu-amd.txt'
-    with open(File, 'r', encoding='utf8') as F:
-        Lines = []
-        for xLine in F.readlines():
-            xLine = xLine.strip()
-            if (xLine) and (not xLine.startswith('-')):
-                Lines.append(xLine)
-
-    for Idx, xLine in enumerate(Lines):
-        print(f'{Idx+1}/{len(Lines)}: {xLine}')
-        for xKey, xVal in SpecObj.items():
-            if (not xKey.startswith('-')):
-                R = xVal.Parse(xLine)
-                if (R):
-                    print(xKey, R)
-        print()
-
-Test_01()
+File = 'Temp/cpu-intel.txt'
+TestAll(File)
