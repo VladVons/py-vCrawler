a024bf947135502d6bababcbd0df9161e542ff6e
VladVons <vladvons@gmail.com>
2024-12-04 17:51:01 +0200

    MyCommit

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@ from Inc.ParserSpec.TestAll import TestAll
 
 
 TimeAt = time.time()
-#File = 'Temp/cpu-intel.txt'
-File = 'Temp/usedpc-1000.txt'
+File = 'Temp/cpu-intel.txt'
+#File = 'Temp/usedpc-1000.txt'
 TestAll(File)
 print(round(time.time() - TimeAt, 2))
