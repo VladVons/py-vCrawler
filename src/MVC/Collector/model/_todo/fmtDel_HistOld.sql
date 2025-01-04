9e12b2bab6017f852a79927faa57c39f3e16fa04
Vladimir Vons <VladVons@gmail.com>
2025-01-04 18:49:13 +0200

    Update fmtDel_HistOld.sql

--- src/MVC/Collector/model/_todo/fmtDel_HistOld.sql
+++ src/MVC/Collector/model/_todo/fmtDel_HistOld.sql
@@ -1,5 +1,4 @@
 -- delete from
--- from
 --     hist_url
 -- where
 --     id not in (
@@ -11,33 +10,20 @@
 --         url_id
 --   );
 
--- with wt1 as (
---     select max(id) as id, url_id 
---     from hist_url
---     group by url_id
---     having count(*) > 1
---     --order by random()
---     limit 100000
--- )
--- delete
--- from hist_url
--- where 
--- 	url_id in (select url_id from wt1) and 
--- 	id not in (select id from wt1)
- 
-delete from
-    hist_url
-where
-    id not in (
-      select
-        max(id)
-      from
-        hist_url
-      group by
-        url_id
-  );
+ with wt1 as (
+     select max(id) as id, url_id 
+     from hist_url
+     group by url_id
+     having count(*) > 1
+     limit 10000
+ )
+ delete 
+ from hist_url hu
+ using wt1
+ where 
+ (hu.url_id = wt1.url_id) and 
+	(hu.id <> wt1.id);
 
 vacuum full hist_url;
-
 vacuum full ref_url;
 vacuum full ref_product;
