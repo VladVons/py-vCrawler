c77df192d67bbdf8afc9f63526ca219847fe384c
Vladimir Vons <VladVons@gmail.com>
2025-01-04 15:14:50 +0200

    Update fmtDel_HistOld.sql

--- src/MVC/Collector/model/_todo/fmtDel_HistOld.sql
+++ src/MVC/Collector/model/_todo/fmtDel_HistOld.sql
@@ -11,6 +11,20 @@
 --         url_id
 --   );
 
+-- with wt1 as (
+--     select max(id) as id, url_id 
+--     from hist_url
+--     group by url_id
+--     having count(*) > 1
+--     --order by random()
+--     limit 100000
+-- )
+-- delete
+-- from hist_url
+-- where 
+-- 	url_id in (select url_id from wt1) and 
+-- 	id not in (select id from wt1)
+ 
 delete from
     hist_url
 where
