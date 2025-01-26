c40c8d5d95c965e0990dc149ea75102667192d92
VladVons <vladvons@gmail.com>
2025-01-26 04:18:43 +0200

    MyCommit

--- src/IncP/__init__.py
+++ src/IncP/__init__.py
@@ -3,8 +3,8 @@
 #License:     GNU, see LICENSE for more details
 
 
-__version__ = '1.0.94'
-__date__ =  '2025.01.25'
+__version__ = '1.0.95'
+__date__ =  '2025.01.26'
 __author__ = 'Vladimir Vons'
 __email__ = 'VladVons@gmail.com'
 __url__ = 'http://oster.com.ua'
--- test/test.py
+++ test/test.py
@@ -239,7 +239,6 @@ async def Main():
     #Url = 'https://1x1.com.ua/product/dell_optiplex_3020_mt_i5-4590_4gb_500gb_hdd_t1'
     #q1 = await UrlGetData_PW(Url)
 
-    #await TSchemer('olx.ua').Test('product')
     #await TSchemer('olx_user.ua').Test('category')
     await TSchemer('olx_shop.ua').Test('category')
     #
