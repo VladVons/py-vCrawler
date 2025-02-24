2bd72854bc9433bb8f67659a24d5f3207bf1960f
VladVons <vladvons@gmail.com>
2025-02-24 18:15:20 +0200

    MyCommit

--- src/MVC/Search/view/assets/jsExt/app.js
+++ src/MVC/Search/view/assets/jsExt/app.js
@@ -1,10 +1,8 @@
 function btnClear(aKey) {
-  document.getElementById('viBtn_Clear').addEventListener('click', function (event) {
     LStorage = new TLocalStorage('products_' + aKey);
     LStorage.remove();
     document.getElementById('viCount_' + aKey).innerHTML = null;
-    document.getElementById('viForm_' + aKey).innerHTML = null;
-  })
+    document.getElementById('viProducts').innerHTML = null;
 }
 
 function OnCountry() {
